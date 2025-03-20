28ef7a14ef40fc8af15d3f12cecb02e1e93a6e35
arthur <arthuriskhakov@mail.ru>
2025-03-20 21:14:13 +0300

    Add parameterized test

--- src/test/java/ru/netology/services/WorkScheduleServiceTest.java
+++ src/test/java/ru/netology/services/WorkScheduleServiceTest.java
@@ -1,44 +1,21 @@
 package ru.netology.services;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 public class WorkScheduleServiceTest {
 
 
-    @Test
-    void shoudCalcMonthsCount1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/data.csv")
+    void shoudCalcMonthsCount(int income, int expenses, int threshold, int expected) {
+
         WorkScheduleService service = new WorkScheduleService();
 
-        // подготавливаем данные:
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        long expected = 3;
-
-        // вызываем целевой метод:
         long actual = service.calculate(income, expenses, threshold);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void shoudCalcMonthsCount2() {
-        WorkScheduleService service = new WorkScheduleService();
-
-        // подготавливаем данные:
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        long expected = 2;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(income, expenses, threshold);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
     }
 }
 
